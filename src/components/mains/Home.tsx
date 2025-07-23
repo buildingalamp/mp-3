2e65a4b095633eec720f754185d73b40a963abaa
jzhou25@bu.edu <jzhou04020@gmail.com>
2025-07-22 22:15:46 -0400

    mp-3 finished

--- src/components/mains/Home.tsx
+++ src/components/mains/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
             <h2>Home</h2>
-            <img src="/../profile.jpg" alt="Jeffrey Zhou"/>
+            <img src="/../../../public/profile.jpg" alt="Jeffrey Zhou"/>
             <p>
                 My name is Jeffrey Zhou. I am a CS student at Boston University. I will be starting law school in the fall.
                 I would like to like to further my knowledge of web design before becoming a lawyer.
